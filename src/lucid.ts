6d1fe28bae6090e2f9ca68ff32c59d6f183ca25e
Sridhar Gudimela <sridhar@lucidact.com>
2023-12-01 07:22:54 -0800

    commented sendResponse

--- src/lucid.ts
+++ src/lucid.ts
@@ -1,12 +1,12 @@
 'use strict';
 import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
-export const sendResponse: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+/*export const sendResponse: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const body ={message: 'Success'};
   return {
     statusCode: 200,
     body
   };
-};
+};*/
 
 export const ping = ():void => {
     console.log('Pinged...');
